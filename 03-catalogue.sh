aff020832df0fec2578f54137a87638381e07cec
AnuragBojja <anuragbojja19@gmail.com>
2026-01-31 16:58:33 -0600

    Shell-Script

--- 03-catalogue.sh
+++ 03-catalogue.sh
@@ -7,6 +7,7 @@ Y="\e[33m"
 N="\e[0m"
 MongoDB_IP="mongodb.anuragaws.shop"
 LOGFOLDER="/var/log/shell-logs"
+SCRIPT_DIR="PWD"
 mkdir -p "$LOGFOLDER"
 LOGFILENAME=$( echo $0 | cut -d "." -f1)
 LOGFILE="$LOGFOLDER/$LOGFILENAME.log"
@@ -62,7 +63,7 @@ VALIDATOR $? "unziped catalogue.zip folder"
 npm install 
 VALIDATOR $? "installed all the dependancies"
 
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 VALIDATOR $? "created catalogue.service file "
 
 systemctl daemon-reload
