bf1edf018a21a9d0cf65350f880f9a5f25699c70
AnuragBojja <anuragbojja19@gmail.com>
2026-01-31 19:48:48 -0600

    Shell-Script

---
+++ 03-catalogue-e.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+ser -e
+# error(){
+
+# }
+trap 'echo "There is an errot in $LINENO , command is $BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MongoDB_IP="mongodb.anuragaws.shop"
+LOGFOLDER="/var/log/shell-logs"
+SCRIPT_DIR="$PWD"
+mkdir -p "$LOGFOLDER"
+LOGFILENAME=$( echo $0 | cut -d "." -f1)
+LOGFILE="$LOGFOLDER/$LOGFILENAME.log"
+echo "Log File Created at $LOGFILE"
+
+if [ "$USERID" -ne 0 ]; then
+    echo "Run this file with Root Privilage" &>> "$LOGFILE"
+    echo "Run this file with Root Privilage"
+    exit 1
+else 
+    echo "This files running with Root Privilage" &>> "$LOGFILE"
+    echo "This files running with Root Privilage"
+fi
+
+# #VALIDATOR(){
+#     if [ "$1" -eq 0 ]; then
+#         echo " $2 SUCCESS" &>> "$LOGFILE"
+#         echo -e "$2 $G SUCCESS $N"
+#     else
+#         echo "ERROR  $2" &>> "$LOGFILE"
+#         echo -e "$R ERROR  $2 $N"
+#         exit 1
+#     fi 
+#     echo " ................................... " &>> "$LOGFILE"
+# }
+
+
+dnf module disable nodejs -y &>> "$LOGFILE"
+#VALIDATOR $? "Disable nodejs modules"
+
+dnf module enable nodejs:20 -y &>> "$LOGFILE"
+#VALIDATOR $? "Enabling Nodejs 20"
+
+dnf install nodejs -y &>> "$LOGFILE"
+#VALIDATOR $? "Installing Nodejs"
+
+id roboshop &>> "$LOGFILE"
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    #VALIDATOR $? "Creating system user roboshop"
+else 
+    echo -e "user roboshop already exiest ......$Y SKIPPING $N"
+fi
+mkdir -p /app 
+#VALIDATOR $? "created /app dir"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> "$LOGFILE"
+#VALIDATOR $? "Downloaded catalogue.zip in tmp folder"
+
+cd /app 
+#VALIDATOR $? "change directory to /app"
+
+rm -rf /app/*
+#VALIDATOR $? "removing existing code"
+
+unzip /tmp/catalogue.zip &>> "$LOGFILE"
+#VALIDATOR $? "unziped catalogue.zip folder"
+
+npm install &>> "$LOGFILE"
+#VALIDATOR $? "installed all the dependancies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+#VALIDATOR $? "created catalogue.service file "
+
+systemctl daemon-reload
+#VALIDATOR $? "completing deamon reload"
+
+systemctl enable catalogue 
+#VALIDATOR $? "enabling catalogue"
+
+systemctl start catalogue
+#VALIDATOR $? "starting catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+#VALIDATOR $? "Adding mongo repo"
+
+dnf install mongodb-mongosh -y &>> "$LOGFILE"
+#VALIDATOR $? "installing mondoDB client"
+
+mongosh --host mongodb.anuragaws.shop --quiet --eval "db.adminCommand('listDatabases').databases.map(db => db.name)" | grep catalogue &>> "$LOGFILE"
+if [ $? -ne 0 ]; then
+    mongosh --host $MongoDB_IP </app/db/master-data.js &>> "$LOGFILE"
+    #VALIDATOR $? "load catalogue products"
+else
+    echo -e "Database Alredy exist......$Y SKIPPING $N"
+fi 
+
+systemctl restart catalogue
+#VALIDATOR $? "restarting catalogue"
