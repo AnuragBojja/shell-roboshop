e165a6fa3d6eb3641bf0483c127a9a854d2b8d4b
AnuragBojja <anuragbojja19@gmail.com>
2026-02-05 16:23:32 -0600

    Shell-Script

---
+++ 06-rabbitMQ.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MongoDB_IP="mongodb.anuragaws.shop"
+LOGFOLDER="/var/log/shell-logs"
+SCRIPT_DIR="$PWD"
+mkdir -p "$LOGFOLDER"
+LOGFILENAME=$( echo $0 | cut -d "." -f1)
+START_TIME=$(date +%s)
+LOGFILE="$LOGFOLDER/$LOGFILENAME.log"
+echo "Log File Created at $LOGFILE"
+
+if [ "$USERID" -ne 0 ]; then
+    echo "Run this file with Root Privilage" &>> "$LOGFILE"
+    echo "Run this file with Root Privilage"
+    exit 1
+else 
+    echo "This files running with Root Privilage" &>> "$LOGFILE"
+    echo "This files running with Root Privilage"
+fi
+
+VALIDATOR(){
+    if [ "$1" -eq 0 ]; then
+        echo " $2 SUCCESS" &>> "$LOGFILE"
+        echo -e "$2 $G SUCCESS $N"
+    else
+        echo "ERROR  $2" &>> "$LOGFILE"
+        echo -e "$R ERROR  $2 $N"
+        exit 1
+    fi 
+    echo " ................................... " &>> "$LOGFILE"
+}
+
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATOR $? "creating rabbitmq.repo"
+dnf install rabbitmq-server -y
+VALIDATOR $? "installing rabbitmq"
+systemctl enable rabbitmq-server
+VALIDATOR $? "enabling rabbitmq"
+systemctl start rabbitmq-server
+VALIDATOR $? "starting rabbitmq"
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATOR $? "setting up permissions"
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "Total time = $TOTAL_TIME"
