e13fab4a8a0d06101eb212db75a167223d69b92b
AnuragBojja <anuragbojja19@gmail.com>
2026-02-05 16:33:25 -0600

    Shell-Script

---
+++ 07-user.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MongoDB_IP="mongodb.anuragaws.shop"
+LOGFOLDER="/var/log/shell-logs"
+SCRIPT_DIR="$PWD"
+mkdir -p "$LOGFOLDER"
+LOGFILENAME=$( echo $0 | cut -d "." -f1)
+LOGFILE="$LOGFOLDER/$LOGFILENAME.log"
+echo "Log File Created at $LOGFILE"
+
+if [ "$USERID" -ne 0 ]; then
+    echo "Run this file with Root Privilage" &>> "$LOGFILE"
+    echo "Run this file with Root Privilage"
+    exit 1
+else 
+    echo "This files running with Root Privilage" &>> "$LOGFILE"
+    echo "This files running with Root Privilage"
+fi
+
+VALIDATOR(){
+    if [ "$1" -eq 0 ]; then
+        echo " $2 SUCCESS" &>> "$LOGFILE"
+        echo -e "$2 $G SUCCESS $N"
+    else
+        echo "ERROR  $2" &>> "$LOGFILE"
+        echo -e "$R ERROR  $2 $N"
+        exit 1
+    fi 
+    echo " ................................... " &>> "$LOGFILE"
+}
+
+
+dnf module disable nodejs -y &>> "$LOGFILE"
+VALIDATOR $? "Disable nodejs modules"
+
+dnf module enable nodejs:20 -y &>> "$LOGFILE"
+VALIDATOR $? "Enabling Nodejs 20"
+
+dnf install nodejs -y &>> "$LOGFILE"
+VALIDATOR $? "Installing Nodejs"
+
+id roboshop &>> "$LOGFILE"
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATOR $? "Creating system user roboshop"
+else 
+    echo -e "user roboshop already exiest ......$Y SKIPPING $N"
+fi
+mkdir -p /app 
+VALIDATOR $? "created /app dir"
+
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>> "$LOGFILE"
+VALIDATOR $? "Downloaded user.zip in tmp folder"
+
+cd /app 
+VALIDATOR $? "change directory to /app"
+
+rm -rf /app/*
+VALIDATOR $? "removing existing code"
+
+unzip /tmp/user.zip &>> "$LOGFILE"
+VALIDATOR $? "unziped user.zip folder"
+
+npm install &>> "$LOGFILE"
+VALIDATOR $? "installed all the dependancies"
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+VALIDATOR $? "created user.service file "
+
+systemctl daemon-reload
+VALIDATOR $? "completing deamon reload"
+
+systemctl enable user 
+VALIDATOR $? "enabling user"
+
+systemctl start user
+VALIDATOR $? "starting user"
+
