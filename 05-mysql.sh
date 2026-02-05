7ec80392f0dbd3c458bb014f7f453b11ee8e0b1b
AnuragBojja <anuragbojja19@gmail.com>
2026-02-05 16:14:53 -0600

    Shell-Script

---
+++ 05-mysql.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MongoDB_IP="mongodb.anuragaws.shop"
+LOGFOLDER="/var/log/shell-logs"
+SCRIPT_DIR="$PWD"
+mkdir -p "$LOGFOLDER"
+LOGFILENAME=$( echo $0 | cut -d "." -f1)
+START_TIME=$(date +%s)
+LOGFILE="$LOGFOLDER/$LOGFILENAME.log"
+echo "Log File Created at $LOGFILE"
+
+if [ "$USERID" -ne 0 ]; then
+    echo "Run this file with Root Privilage" &>> "$LOGFILE"
+    echo "Run this file with Root Privilage"
+    exit 1
+else 
+    echo "This files running with Root Privilage" &>> "$LOGFILE"
+    echo "This files running with Root Privilage"
+fi
+
+VALIDATOR(){
+    if [ "$1" -eq 0 ]; then
+        echo " $2 SUCCESS" &>> "$LOGFILE"
+        echo -e "$2 $G SUCCESS $N"
+    else
+        echo "ERROR  $2" &>> "$LOGFILE"
+        echo -e "$R ERROR  $2 $N"
+        exit 1
+    fi 
+    echo " ................................... " &>> "$LOGFILE"
+}
+dnf install mysql-server -y
+VALIDATOR $? "installing mySQL Server SUCCESS"
+systemctl enable mysqld
+VALIDATOR $? "Enabling mySQL SUCCESS"
+systemctl start mysqld  
+VALIDATOR $? "Starting mySQL SUCCESS"
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATOR $? "setting root password SUCCESS"
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "Total time = $TOTAL_TIME"
